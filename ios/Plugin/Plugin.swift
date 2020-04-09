a719f53ed8ae69e08d8f6ef8417e9360bd5d1a5f
snuffy <snuffy.tokyo@gmail.com>
2020-04-09 15:27:26 +0900

    setup initialize function

--- ios/Plugin/Plugin.swift
+++ ios/Plugin/Plugin.swift
@@ -1,13 +1,21 @@
 import Foundation
 import Capacitor
+import GoogleSignIn
 
-/**
- * Please read the Capacitor iOS Plugin Development Guide
- * here: https://capacitor.ionicframework.com/docs/plugins/ios
- */
+
 @objc(Sso)
 public class Sso: CAPPlugin {
+    let googleSignIn: GIDSignIn = GIDSignIn.sharedInstance();
     
+    override public func load() {
+        let googleClientId = getConfigValue("googleClientId") as? String ?? "ADD_IN_CAPACITOR_CONFIG_JSON"
+        
+        self.googleSignIn.clientID = googleClientId
+        self.googleSignIn.delegate = self as? GIDSignInDelegate
+        self.googleSignIn.presentingViewController = bridge.viewController
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(notifyFromAppDelegate(notification:)), name: Notification.Name(CAPNotifications.URLOpen.name()), object: nil);
+    }
     @objc func echo(_ call: CAPPluginCall) {
         let value = call.getString("value") ?? ""
         call.success([
